28e6abc9465c883bb67e3935fc900163136c375d
zhouhuafei <1123486116@qq.com>
2018-03-11 17:21:14 +0800

    delegate

--- dist/index.js
+++ dist/index.js
@@ -3,14 +3,34 @@
 var getDomArray = require('zhf.get-dom-array');
 
 var eventDelegate = {
-    on: function on(parentElement, currentElement, eventType, fn) {
+    on: function on(parentElement, currentElement) {
+        var eventType = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'click';
+        var fn = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : function () {};
+
         var parent = getDomArray(parentElement)[0];
         var current = getDomArray(currentElement)[0];
         if (parent && current) {
-            parent.addEventListener('click', function () {});
+            parent.addEventListener(eventType, function (ev) {
+                ev = ev || window.event;
+                var target = ev.target || ev.srcElement;
+                var isParent = false; // 是否冒泡到了父级
+                while (target !== current && !isParent) {
+                    target = target.parentNode;
+                    if (target === parent) {
+                        isParent = true;
+                    }
+                }
+                if (target === current) {
+                    fn();
+                }
+            });
         }
     },
-    remove: function remove() {}
+    remove: function remove(parentElement, currentElement) {
+        var eventType = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'click';
+
+        parentElement.removeEventListener(eventType);
+    }
 };
 
 module.exports = eventDelegate;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 const getDomArray = require('zhf.get-dom-array');
 
 const eventDelegate = {
-    on(parentElement, currentElement, eventType, fn) {
+    on(parentElement, currentElement, eventType = 'click', fn = function () {
+    }) {
         const parent = getDomArray(parentElement)[0];
         const current = getDomArray(currentElement)[0];
         if (parent && current) {
-            parent.addEventListener('click', function () {
-
+            parent.addEventListener(eventType, function (ev) {
+                ev = ev || window.event;
+                let target = ev.target || ev.srcElement;
+                let isParent = false; // 是否冒泡到了父级
+                while (target !== current && !isParent) {
+                    target = target.parentNode;
+                    if (target === parent) {
+                        isParent = true;
+                    }
+                }
+                if (target === current) {
+                    fn();
+                }
             });
         }
     },
-    remove() {
+    remove(parentElement, currentElement, eventType = 'click') {
+        parentElement.removeEventListener(eventType);
     },
 };
 
